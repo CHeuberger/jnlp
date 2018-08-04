a6879424048a4748da4d8aa7f415704898f7604b
Carlos Heuberger <fursgyv.cfh@dfgh.net>
2018-08-04 08:38:39 +0000

    #1 Test Application: JOptionPane

--- src/java/cfh/test/jnlp/Main.java
+++ src/java/cfh/test/jnlp/Main.java
@@ -1,8 +1,12 @@
 package cfh.test.jnlp;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("started");
+        
+        JOptionPane.showMessageDialog(null, "started");
     }
 }
